72e0c88c2fae0891460065c6e907025bb3116000
Changbai Li <changbai.li@heliosinteractive.com>
2016-06-15 17:25:36 -0700

    WIP

---
+++ app.js
@@ -0,0 +1,108 @@
+var _ = require('underscore');
+var async = require('async');
+var bodyParser = require('body-parser');
+var config = require('config');
+var express = require('express');
+var expressWs = require('express-ws');
+var morgan = require('morgan');
+var neuralStyleRenderer = require('./neural-style-renderer');
+var neuralStyleUtil = require('./neural-style-util');
+var passport = require('passport');
+var passportHttp = require('passport-http');
+
+var app = express();
+expressWs(app);
+app.use(morgan('short'));
+
+if (config.has('username') && config.has('password')) {
+  passport.use(new passportHttp.BasicStrategy(
+    function(username, password, callback) {
+      if (username != config.get('username') || password != config.get('password')) {
+        callback(null, false);
+      }
+      callback(null, {});
+    }));
+  app.use(passport.authenticate('basic', {session: false}));
+}
+
+app.use(express.static('public'));
+app.use('/data', express.static(config.get('dataPath')));
+
+// var rawBodyParser = bodyParser.raw({limit: '10mb'});
+// app.post('/upload/:id/:purpose/:index', rawBodyParser, function(req, res) {
+//   if (!neuralStyleUtil.validateId(req.params.id)) {
+//     res.status(400).send('invalid id');
+//     return;
+//   }
+//   neuralStyleUtil.saveImage(req.params.id, req.params.purpose, req.params.index, req.body, function(err) {
+//     if (err) {
+//       res.status(500).send(err);
+//       return;
+//     }
+//     res.end();
+//   });
+// });
+
+// var jsonBodyParser = bodyParser.json();
+// app.post('/render/:id', jsonBodyParser, function(req, res) {
+//   if (!neuralStyleUtil.validateId(req.params.id)) {
+//     res.status(400).send('invalid id');
+//     return;
+//   }
+//   var settings = req.body;
+//   neuralStyleRenderer.enqueueJob(req.params.id, settings);
+//   res.end();
+// });
+
+// app.post('/cancel/:id', function(req, res) {
+//   if (!neuralStyleUtil.validateId(req.params.id)) {
+//     res.status(400).send('invalid id');
+//     return;
+//   }
+//   neuralStyleRenderer.cancelJob(req.params.id);
+//   res.end();
+// });
+
+// var updateSockets = [];
+// app.ws('/updates', function(ws, req) {
+//   updateSockets.push(ws);
+//   ws.on('close', function() {
+//     var index = updateSockets.indexOf(ws);
+//     updateSockets.splice(index, 1);
+//   });
+//   neuralStyleRenderer.getStatus(function(status) {
+//     if (_.findIndex(updateSockets, ws) == -1) {
+//       return;
+//     }
+//     ws.send(JSON.stringify({'type': 'status', 'data': status}));
+//   });
+//   process.nextTick(function() {
+//     if (_.findIndex(updateSockets, ws) == -1) {
+//       return;
+//     }
+//     var taskStatuses = neuralStyleRenderer.getTaskStatuses();
+//     for (var i = taskStatuses.length - 1; i >= 0; i--) {
+//       ws.send(JSON.stringify({'type': 'render', 'data': taskStatuses[i]}));
+//     }
+//   });
+// });
+
+// function broadcastUpdate(type, data) {
+//   _.each(updateSockets, function(ws) {
+//     ws.send(JSON.stringify({'type': type, 'data': data}));
+//   });
+// }
+
+// neuralStyleRenderer.eventEmitter.on('render', function(taskStatus) {
+//   broadcastUpdate('render', taskStatus);
+// });
+//
+// neuralStyleRenderer.eventEmitter.on('status', function(status) {
+//   broadcastUpdate('status', status);
+// });
+
+var server = app.listen(config.get('port'), function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://%s:%s', host, port);
+});
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@ Neural Style Batch Runner
 ========
 
 An interface to run a batch of neural style transfers at once.
+
+Current Goal: Be able to run with the same content and style images, but varying combinations of parameters.
+
+- Calculate and show number of tasks based on parameter combination
+  - These would scale, so be careful...
+  - is it possible to calculate an estimate of how long they'll take?
+- Option to cancel the batch
+- 
